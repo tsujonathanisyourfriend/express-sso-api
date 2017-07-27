f5ff2ab82416abb096d2a13f4d2bfb570f4c0d18
hxu <hxu@teksystems.com>
2017-07-26 23:36:56 -0400

    Add JWT take 1

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,19 @@
 import * as express from 'express';
+import * as morgan from 'morgan';
 import { ICustomerRoute, CustomerRoute } from './routes/customer-route';
 import { ICustomerService, CustomerService } from './services/customer-service';
-import * as morgan from 'morgan';
 import { CustomerRepository } from './repositories/customer-repository';
 
-let app = express();
-let port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 
-app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
-    //console.log('Application level middleware. Time:', Date.now());
-    next();
-});
-
-var customerRepository: CustomerRepository = new CustomerRepository();
-let customerService: ICustomerService = new CustomerService(customerRepository);
-let customerRoute: ICustomerRoute = new CustomerRoute(customerService);
+const customerRepository: CustomerRepository = new CustomerRepository();
+const customerService: ICustomerService = new CustomerService(customerRepository);
+const customerRoute: ICustomerRoute = new CustomerRoute(customerService);
 app.get('/customers/:id', customerRoute.getCustomers);
+app.get('/authenticate', customerRoute.authenticate);
 
 app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
     console.error(err.stack);
--- src/routes/customer-route.ts
+++ src/routes/customer-route.ts
@@ -1,19 +1,30 @@
 import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
 import { ICustomerService } from '../services/customer-service';
 
 export interface ICustomerRoute {
     getCustomers(req: express.Request, res: express.Response): void;
+    authenticate(req: express.Request, res: express.Response): void;
 }
 
 export class CustomerRoute implements ICustomerRoute {
     public constructor(private customerService: ICustomerService) { }
-                                                
-    public getCustomers = (req: express.Request, res: express.Response): void => {
-        //console.log('Router level middleware. Time:', Date.now());
 
+    public getCustomers = (req: express.Request, res: express.Response): void => {
         const id = req.params['id'];
-        this.customerService.getCustomers(+id).then(p=>{
+        this.customerService.getCustomers(+id).then(p => {
             res.json(p);
+        });
+    };
+
+    public authenticate = (req: express.Request, res: express.Response): void => {
+        var token = jwt.sign('user', 'superSecret');
+
+        // return the information including token as JSON
+        res.json({
+            success: true,
+            message: 'Enjoy your token!',
+            token: token
         });
     };
 }
